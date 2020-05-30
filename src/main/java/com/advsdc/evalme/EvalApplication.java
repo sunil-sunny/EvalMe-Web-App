22ca250206033749a8c3009ab18e903f98205334
karthikktamilmani <t.karthikk10@gmail.com>
2020-05-30 13:38:59 -0700

    initial commit

---
+++ src/main/java/com/advsdc/evalme/EvalApplication.java
@@ -0,0 +1,13 @@
+package com.advsdc.evalme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advsdc/evalme/EvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advsdc.evalme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
