62c9f75f95140365bd2eecdd35c56f74f8d1beb5
sunil-sunny <sunilsunny183@gmail.com>
2020-05-30 21:22:36 -0300

    changed to maven

---
+++ src/test/java/com/groupformationtool/GroupFormationTool/GroupFormationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupformationtool.GroupFormationTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupFormationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
