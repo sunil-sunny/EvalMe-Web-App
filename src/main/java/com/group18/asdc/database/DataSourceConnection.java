d9b03aae11511674d28a78e0ea6d15d01e9e8cb8
sunil-sunny <sunilsunny183@gmail.com>
2020-05-31 14:08:38 -0300

    Data Source issue fixed

--- src/main/java/com/group18/asdc/database/DataSourceConnection.java
+++ src/main/java/com/group18/asdc/database/DataSourceConnection.java
@@ -1,12 +1,8 @@
 package com.group18.asdc.database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import javax.annotation.sql.DataSourceDefinition;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,42 +13,14 @@ import org.springframework.context.annotation.Profile;
 @ConfigurationProperties("spring.datasource")
 public class DataSourceConnection {
 
-    private String driverClassName;
+	@Value("${spring.database.drivername}")
+	private String driverClassName;
+	@Value("${spring.database.dev.url}")
 	private String url;
+	@Value("${spring.database.dev.username}")
 	private String username;
+	@Value("${spring.database.dev.password}")
 	private String password;
-
-	public String getDriverClassName() {
-		return driverClassName;
-	}
-
-	public void setDriverClassName(String driverClassName) {
-		this.driverClassName = driverClassName;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	@Profile("dev")
 	@Bean
@@ -79,16 +47,17 @@ public class DataSourceConnection {
 		System.out.println(driverClassName);
 		System.out.println(url);
 		return "DB Connection to RDS_PROD - High Performance Instance";
-	} 
+	}
 
-    // @Bean
-    // public DataSource getDataSource() {
-    //     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-    //     dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-    //     dataSourceBuilder.url("jdbc:mysql://localhost:3306/beforeNorm?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-    //     dataSourceBuilder.username("myuser");
-    //     dataSourceBuilder.password("password");
-    //     return dataSourceBuilder.build();
-    // }
-    
+	@Bean
+	public DataSource getDataSource() {
+		
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+		dataSourceBuilder.url(url);
+		dataSourceBuilder.username(username);
+		dataSourceBuilder.password(password);
+		return dataSourceBuilder.build();
+	}
+
 }
