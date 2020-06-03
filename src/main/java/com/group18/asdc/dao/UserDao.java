3e0684aaff225e567fa667fcbb0f09bfca96eed9
karthikktamilmani <t.karthikk10@gmail.com>
2020-06-02 17:36:38 -0700

    duplicate import fix

--- src/main/java/com/group18/asdc/dao/UserDao.java
+++ src/main/java/com/group18/asdc/dao/UserDao.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import com.group18.asdc.entities.User;
 
-import com.group18.asdc.entities.User;
-
 public interface UserDao {
     
     Boolean authenticateByEmailAndPassword(ArrayList<Object> valueList) throws SQLException;
