4f47ae699f4cfccedb47d9f817fd730eb53150e7
sunil-sunny <sunilsunny183@gmail.com>
2020-06-03 20:05:34 -0300

    Changed data source class file,removed unwanted methods in few pcakages along with some added tests

--- src/test/java/com/group18/asdc/dao/test/UserDaoImplMock.java
+++ src/test/java/com/group18/asdc/dao/test/UserDaoImplMock.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import com.group18.asdc.dao.UserDao;
 import com.group18.asdc.entities.Course;
 import com.group18.asdc.entities.User;
