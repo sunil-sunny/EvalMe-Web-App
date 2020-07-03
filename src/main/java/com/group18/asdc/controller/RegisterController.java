63a39488c977bbb63152395446827cd9a1fe372d
sunil-sunny <sunilsunny183@gmail.com>
2020-07-03 16:51:42 -0300

    Code refactor and Code cleaning performed

--- src/main/java/com/group18/asdc/controller/RegisterController.java
+++ src/main/java/com/group18/asdc/controller/RegisterController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.group18.asdc.SystemConfig;
 import com.group18.asdc.entities.PasswordHistory;
 import com.group18.asdc.entities.UserRegistartionDetails;
 import com.group18.asdc.service.PasswordHistoryService;
 import com.group18.asdc.service.RegisterService;
+import com.group18.asdc.util.RegistrationStatus;
 
 
 @Controller
