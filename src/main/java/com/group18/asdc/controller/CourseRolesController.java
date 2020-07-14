80b61ffe42d6d19f4dca61014d53588e37adc9b2
sunil-sunny <sunilsunny183@gmail.com>
2020-07-14 18:54:15 -0300

    Changed Business logic of upload csv to service layer

--- src/main/java/com/group18/asdc/controller/CourseRolesController.java
+++ src/main/java/com/group18/asdc/controller/CourseRolesController.java
@@ -1,27 +1,19 @@
 package com.group18.asdc.controller;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.group18.asdc.CourseConfig;
 import com.group18.asdc.ProfileManagementConfig;
 import com.group18.asdc.entities.User;
+import com.group18.asdc.errorhandling.FileProcessingException;
 import com.group18.asdc.service.CourseRolesService;
 import com.group18.asdc.service.UserService;
-import com.group18.asdc.util.ConstantStringUtil;
 
 @Controller
 public class CourseRolesController {
@@ -59,61 +51,24 @@ public class CourseRolesController {
 		theModel.addAttribute("courseId", courseId);
 		theModel.addAttribute("coursename", courseName);
 		CourseRolesService courseRolesService = CourseConfig.getSingletonInstance().getTheCourseRolesService();
-		if (0 == courseId.length()) {
-			theModel.addAttribute("resultEnrolling", "Error in loading file !! please try again");
-		} else {
-			if (file.isEmpty()) {
-				theModel.addAttribute("resultEnrolling", "Upload file to continue");
-			} else {
-				try {
-					byte[] bytes = file.getBytes();
-					ByteArrayInputStream inputFilestream = new ByteArrayInputStream(bytes);
-					BufferedReader br = new BufferedReader(new InputStreamReader(inputFilestream));
-					String line = "";
-					br.readLine();
-					List<User> validUsers = new ArrayList<User>();
-					List<User> inValidUsers = new ArrayList<User>();
-					User user = null;
-					while ((line = br.readLine()) != null) {
-						user = new User();
-						String userDetails[] = line.split(",");
-						if (userDetails.length == 4) {
-							String firstName = userDetails[0];
-							String lastName = userDetails[1];
-							String bannerId = userDetails[2];
-							String email = userDetails[3];
-
-							if (bannerId.matches(ConstantStringUtil.BANNER_ID_CHECK.toString())
-									|| bannerId.length() == 9
-									|| email.matches(ConstantStringUtil.EMAIL_PATTERN_CHECK.toString())) {
-								user.setFirstName(firstName);
-								user.setLastName(lastName);
-								user.setBannerId(bannerId);
-								user.setEmail(email);
-								validUsers.add(user);
-							} else {
-								inValidUsers.add(user);
-							}
-						} else {
-							theModel.addAttribute("fileDetailsErrors", "Rows which has invalid details are ignored");
-						}
-					}
-					if (validUsers.size() > 0) {
-						boolean status = courseRolesService.enrollStuentsIntoCourse(validUsers,
-								Integer.parseInt(courseId));
-						if (status) {
-							theModel.addAttribute("resultEnrolling", "All Students enrolled");
-						} else {
-							theModel.addAttribute("resultEnrolling",
-									"Success!! " + "Users who are already related to course are ignored");
-						}
-					}
-					br.close();
-				} catch (IOException e) {
-					theModel.addAttribute("resultEnrolling", "File is not readable, Kinldy upload it again");
+		List<User> validUsers;
+		try {
+			validUsers = courseRolesService.extraxtValidStudentsFromFile(file);
+			if (validUsers.size() > 0) {
+				boolean status = courseRolesService.enrollStuentsIntoCourse(validUsers, Integer.parseInt(courseId));
+				if (status) {
+					theModel.addAttribute("resultEnrolling", "All Students enrolled");
+				} else {
+					theModel.addAttribute("resultEnrolling", "Students are not Enrolled into course, Kinldy try again");
 				}
+			} else {
+				theModel.addAttribute("resultEnrolling",
+						"Students in the file has invalid details, Kindly Correct them and try again");
 			}
+		} catch (FileProcessingException e) {
+			theModel.addAttribute("resultEnrolling", e.getMessage());
 		}
+
 		return "instrcutorcoursehome";
 	}
 }
--- src/main/java/com/group18/asdc/service/CourseRolesServiceImpl.java
+++ src/main/java/com/group18/asdc/service/CourseRolesServiceImpl.java
@@ -1,12 +1,20 @@
 package com.group18.asdc.service;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.group18.asdc.CourseConfig;
 import com.group18.asdc.ProfileManagementConfig;
 import com.group18.asdc.dao.CourseRolesDao;
 import com.group18.asdc.entities.User;
+import com.group18.asdc.errorhandling.FileProcessingException;
+import com.group18.asdc.util.ConstantStringUtil;
 
 public class CourseRolesServiceImpl implements CourseRolesService {
 
@@ -42,5 +50,50 @@ public class CourseRolesServiceImpl implements CourseRolesService {
 		} else {
 			return Boolean.FALSE;
 		}
+	}
+
+	@Override
+	public List<User> extraxtValidStudentsFromFile(MultipartFile file) throws FileProcessingException {
+		List<User> validUsers = new ArrayList<User>();
+		List<User> inValidUsers = new ArrayList<User>();
+		if (file.isEmpty()) {
+			throw new FileProcessingException("uploaded file is empty, Kindly upload valid file");
+		} else {
+			try {
+				byte[] bytes = file.getBytes();
+				ByteArrayInputStream inputFilestream = new ByteArrayInputStream(bytes);
+				BufferedReader br = new BufferedReader(new InputStreamReader(inputFilestream));
+				String line = "";
+				br.readLine();
+				User user = null;
+				while (null != (line = br.readLine())) {
+					user = new User();
+					String userDetails[] = line.split(",");
+					if (4 == userDetails.length) {
+						String firstName = userDetails[0];
+						String lastName = userDetails[1];
+						String bannerId = userDetails[2];
+						String email = userDetails[3];
+						if (bannerId.matches(ConstantStringUtil.BANNER_ID_CHECK.toString()) || bannerId.length() == 9
+								|| email.matches(ConstantStringUtil.EMAIL_PATTERN_CHECK.toString())) {
+							user.setFirstName(firstName);
+							user.setLastName(lastName);
+							user.setBannerId(bannerId);
+							user.setEmail(email);
+							validUsers.add(user);
+						} else {
+							inValidUsers.add(user);
+						}
+					} else {
+						throw new FileProcessingException(
+								"Content in the file doesnt match the format kinldy rectify it ang upload again");
+					}
+				}
+				br.close();
+			} catch (IOException e) {
+				throw new FileProcessingException("File doesnt exist or it is invalid, Kinldy try again");
+			}
+		}
+		return validUsers;
 	}
 }
