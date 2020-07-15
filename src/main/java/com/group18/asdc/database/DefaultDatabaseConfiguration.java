9b29315f8c0dd2d975bc59f055a7e5d8086c99e8
mani <kr630601@dal.ca>
2020-07-15 16:31:46 -0700

    default db values

--- src/main/java/com/group18/asdc/database/DefaultDatabaseConfiguration.java
+++ src/main/java/com/group18/asdc/database/DefaultDatabaseConfiguration.java
@@ -2,7 +2,7 @@ package com.group18.asdc.database;
 
 public class DefaultDatabaseConfiguration implements IDatabaseConfiguration {
 
-	private String url = ""; //System.getenv("DB_URL");
+	private String url = System.getenv("DB_URL");
 	private String username = System.getenv("DB_USERNAME");
 	private String password = System.getenv("DB_PASSWORD");
 
