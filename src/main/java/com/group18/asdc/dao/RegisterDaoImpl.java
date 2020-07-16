0b789f18a8b49303c3624fdf698b1a2031ad8fd2
sunil-sunny <sunilsunny183@gmail.com>
2020-07-16 16:39:14 -0300

    Fixed query error in survey queries

--- src/main/java/com/group18/asdc/dao/RegisterDaoImpl.java
+++ src/main/java/com/group18/asdc/dao/RegisterDaoImpl.java
@@ -50,7 +50,8 @@ public class RegisterDaoImpl implements RegisterDao {
 				isGuestRoleAssigned = Boolean.TRUE;
 			}
 			if (isGuestRoleAssigned && isUserRegisterd) {
-				log.log(Level.INFO,"User with id " + registerDetails.getBannerid() + " has been successfully registered");
+				log.log(Level.INFO,
+						"User with id " + registerDetails.getBannerid() + " has been successfully registered");
 				connection.commit();
 			} else {
 				log.log(Level.WARNING, "User with id " + registerDetails.getBannerid() + " has not been registered");
