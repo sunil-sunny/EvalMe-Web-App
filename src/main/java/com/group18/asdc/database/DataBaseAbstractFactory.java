50fe142095b3e2533a24ee5cc5716fc8fd9e42a5
mani <kr630601@dal.ca>
2020-07-16 03:11:25 -0700

    factory for sqlmethods

--- src/main/java/com/group18/asdc/database/DataBaseAbstractFactory.java
+++ src/main/java/com/group18/asdc/database/DataBaseAbstractFactory.java
@@ -1,6 +1,10 @@
 package com.group18.asdc.database;
 
+import java.sql.Connection;
+
 public interface DataBaseAbstractFactory {
 
 	public IDatabaseConfiguration getDatabaseConfiguration();
+
+	public ISQLMethods getSQLMethods(Connection connection);
 }
